4ec11c355d0f993b146dc9d273fbc77f7f1f8648
javimetro <javierj@metropolia.fi>
2022-08-26 13:37:12 +0300

    tehtävä4

---
+++ moduuli2/tehtävä4.py
@@ -0,0 +1,13 @@
+luku1_str = input('kirjoita ensimmainen luku ')
+luku1 = float(luku1_str)
+luku2_str = input('kirjoita toinen luku ')
+luku2 = float(luku2_str)
+luku3_str = input('kirjoita kolmas luku ')
+luku3 = float(luku3_str)
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa/3
+print(summa)
+print(tulo)
+print(keskiarvo)
+
