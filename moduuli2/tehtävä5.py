a8307fc94f00d3cfbe11885190e82f73f96e6a2e
javimetro <javierj@metropolia.fi>
2022-08-26 17:16:13 +0300

    tehtävä5

--- moduuli2/tehtävä5.py
+++ moduuli2/tehtävä5.py
@@ -6,18 +6,19 @@ LEIVISKA = 20 * NAULA
 leiviskat = input('Montako leiviskaa: ')
 leiviskatGrammoina = float(leiviskat) * LEIVISKA
 print(leiviskatGrammoina)
-#kysy montako naula
+
 naulat = input('montako naulaa: ')
 naulatGrammoina = float(naulat) * NAULA
 print(naulatGrammoina)
-#kysy montako luotia
+
 luotit = input('montako luotia: ')
 luotitGrammoina = float(luotit) * LUOTI
 print(luotitGrammoina)
-#kerro annetut luvut LUOTI, NAULA ja  LEIVISKÄ muuttujien kanssa
+
 Kokonaipaino = luotitGrammoina + naulatGrammoina + leiviskatGrammoina
 print(Kokonaipaino)
+kilot = float(math.floor(Kokonaipaino/1000))
 print('Kokonaispaino grammoina on '+ str(Kokonaipaino))
-print('Massa nykymittojen mukaan kilogrammoina: '+ str(math.floor(Kokonaipaino/1000)))
-print('Massa nykymittojen mukaan grammoinna: ' + str(math.floor()))
-
+print('Massa nykymittojen mukaan kilogrammoina: '+ str(kilot))
+grammat = float(math.floor(Kokonaipaino%1000))
+print('Massa nykymittojen mukaan grammoinna: ' + str(grammat))
