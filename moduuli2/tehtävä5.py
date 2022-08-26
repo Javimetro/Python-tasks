46e40ff79bb64f1e02854664b710fc7202358283
javimetro <javierj@metropolia.fi>
2022-08-26 12:00:38 +0300

    tehtävä5

---
+++ moduuli2/tehtävä5.py
@@ -0,0 +1,18 @@
+LUOTI = 13,3
+NAULA = 32* luoti
+LEIVISKA = 20 * naula
+
+#kysy montako leiviskää
+
+leiviskat = input('Montako leiviskaa: ')
+leiviskatGrammoina = float(leiviskat) * LEIVISKA
+print(leiviskatGrammoina)
+#kysy montako naula
+
+#kysy montako luotia
+
+#kerro annetut luvut LUOTI, NAULA ja  LEIVISKÄ muuttujien kanssa
+
+#laske kerrotut luvut yhteen -> kokonaispainoa grammoina
+
+# erota kilot ja grammat
