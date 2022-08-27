765ee572c0ab103f247c3c81104c329c6de4c7ff
javimetro <javierj@metropolia.fi>
2022-08-27 10:15:27 +0300

    tehtävä6

---
+++ moduuli2/tehtävä6.py
@@ -0,0 +1,8 @@
+import random
+numbers = '0123456789'
+lenght3 = 3
+lenght4 = 4
+kolmenumeroinenKoodi = (random.sample(numbers,lenght3))
+nelinumeroinenKoodi = (random.sample(numbers,lenght4))
+print('Sinun kolmenumeroinen koodi on: ' + str(kolmenumeroinenKoodi))
+print('Sinun nelinumeroinen koodi on: ' + str (nelinumeroinenKoodi))
