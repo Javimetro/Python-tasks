a7e077ae13850c6ff9c5a883a3e276d34cb4afcb
javimetro <javierj@metropolia.fi>
2022-08-29 10:54:35 +0300

    tehtävä3m3

---
+++ moduuli3/Tehtävä3m3.py
@@ -0,0 +1,20 @@
+
+#Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
+#Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
+
+sukupuoli = input('Mitä on sinun sukupuolesi?: ')
+hb = int(input('Kirjoita hemoglobiiniarvosi (g/l): '))
+
+if sukupuoli=='Mies' or sukupuoli=='mies' and 134<=hb<195:
+    print('Sinun hemoglobiiniarvo on normaali.')
+if sukupuoli=='Mies' or sukupuoli=='mies' and 134>hb:
+    print('Sinun hemoglobiiniarvo on matala.')
+if sukupuoli=='Mies' or sukupuoli=='mies' and 195<hb:
+    print('Sinun hemoglobiiniarvo on korkea.')
+
+if sukupuoli=='Nainen' or sukupuoli=='nainen' and 117<=hb<175:
+    print('Sinun hemoglobiiniarvo on normaali.')
+if sukupuoli=='Nainen' or sukupuoli=='nainen' and 117>hb:
+    print('Sinun hemoglobiiniarvo on matalia.')
+if sukupuoli=='Nainen' or sukupuoli=='nainen' and 175<hb:
+    print('Sinun hemoglobiiniarvo on korkea.')
