e3a145d7a2b4b9a2b9e7c05f9ff9f14ed1a7b216
Javi <111982831+Javimetro@users.noreply.github.com>
2022-09-09 11:38:02 +0300

    Update tehtävä3_alkuluku.py
    kieliopivirhe korjattu

--- moduuli5/tehtävä3_alkuluku.py
+++ moduuli5/tehtävä3_alkuluku.py
@@ -1,13 +1,13 @@
-luku = int(input('Anna kokonaisluvun: '))
+luku = int(input('Anna kokolaisluvun: '))
 
 if luku == 1:
-    print('ei ole kokonaisluku')
+    print('ei ole alkuluku')
 elif luku == 2:
-    print('on kokonaisluku')
+    print('on alkuluku')
 else:
     for i in range (2,luku):
         if luku % i == 0:
-            print('ei ole kokonaisluku')
+            print('ei ole alkuluku')
             break
     else:
-        print('On kokonaisluku')
+        print('On alkuluku')
